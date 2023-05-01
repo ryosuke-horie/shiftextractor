e725de4a700d70178832d4366789df13cba47261
ryosuke-horie <ryosuke03140@gmial.com>
2023-05-01 19:48:01 +0900

    PDFを画像に変換

---
+++ main.py
@@ -0,0 +1,27 @@
+from google.cloud import vision
+import io
+
+def detect_text(path):
+    """Detects text in the file located in Google Cloud Storage or on the Web."""
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+file_path = "path/to/your/image.jpg"
+detect_text(file_path)
---
+++ src/pdf_to_image.py
@@ -0,0 +1,26 @@
+# PDFファイルを画像に変換する
+
+from pdf2image import convert_from_path # pdfを画像に変換する
+import os                               # ファイルパスを扱う
+
+# PDFファイルを画像に変換する
+def convert_pdf_to_images(pdf_file_path, output_folder, output_format='jpeg'):
+    # PDFファイルを画像に変換
+    images = convert_from_path(pdf_file_path)
+
+    # 画像ファイルを保存
+    image_file_paths = []
+    for i, image in enumerate(images):
+        image_file_path = f"{output_folder}/output_{i}.{output_format}"
+        image.save(image_file_path, output_format.upper())
+        image_file_paths.append(image_file_path)
+
+    return image_file_paths
+
+# パスを設定
+current_dir   = os.path.dirname(os.path.abspath(__file__))
+pdf_file_path = os.path.join(current_dir, '..', 'pdf', '202304.pdf') # 画像に変換したいPDFファイルのパス
+output_folder = os.path.join(current_dir, '..', 'images')            # 画像を保存するフォルダのパス
+
+# PDFファイルを画像に変換
+image_file_paths = convert_pdf_to_images(pdf_file_path, output_folder)
